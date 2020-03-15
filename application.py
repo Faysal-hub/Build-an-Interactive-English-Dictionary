255c654fdede625fa9a52cb0aa0f084feffd60f5
Faysal-hub <ovitahsan@gmail.com>
2020-03-15 20:59:33 +0100

    the definition of acronyms

--- application.py
+++ application.py
@@ -5,21 +5,35 @@ data = json.load(open("data.json"))
  
 def translate(myInput):  
     myInput = myInput.lower()
+    
     if myInput in data:
         return data[myInput]
+
+    elif myInput.title() in data:
+        return data[myInput.title()]
+
+    elif myInput.upper() in data: #in case user enters words like USA or NATO
+        return data[myInput.upper()]
+
     elif len(get_close_matches(myInput, data.keys())) > 0:
         myOpenion = input(f"Did you mean {get_close_matches(myInput, data.keys())[0]} instead? Enter Y if yes, or N if no: ")
+        
         if myOpenion == "Y":
             return data[get_close_matches(myInput, data.keys())[0]]
+        
         elif myOpenion == "N":
             return "The word doesn't exist. Please double check it."
+        
         else:
             return "We didn't understand your entry."
+    
     else:
         return "The word doesn't exist. Please double check it."
 
+
 word = input("Enter word: ")
 output = translate(word)
+
 if type(output) == list:
     for item in output:
         print(item)
