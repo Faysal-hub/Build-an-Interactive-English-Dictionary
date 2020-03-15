8c43c3832ab50fbaa1931ed77abfe5922dad255a
Faysal-hub <ovitahsan@gmail.com>
2020-03-15 14:40:06 +0100

    complete project

---
+++ README.txt
@@ -0,0 +1,1 @@
+I build an Interactive English Dictionary. Here i have JSON file. From the terminal, when i enter a word, it will provide us the meaning of the word.
---
+++ application.py
@@ -0,0 +1,27 @@
+import json
+from difflib import get_close_matches
+
+data = json.load(open("data.json"))
+ 
+def translate(myInput):  
+    myInput = myInput.lower()
+    if myInput in data:
+        return data[myInput]
+    elif len(get_close_matches(myInput, data.keys())) > 0:
+        myOpenion = input(f"Did you mean {get_close_matches(myInput, data.keys())[0]} instead? Enter Y if yes, or N if no: ")
+        if myOpenion == "Y":
+            return data[get_close_matches(myInput, data.keys())[0]]
+        elif myOpenion == "N":
+            return "The word doesn't exist. Please double check it."
+        else:
+            return "We didn't understand your entry."
+    else:
+        return "The word doesn't exist. Please double check it."
+
+word = input("Enter word: ")
+output = translate(word)
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
